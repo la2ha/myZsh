7f211fe979a343c1d443f8b20b2d62565f421085
La2ha <la2ha@la2ha.com>
2015-06-10 00:10:30 +0300

    step 2

--- start.sh
+++ start.sh
@@ -4,3 +4,10 @@ sudo apt-get update
 sudo apt-get -y install zsh
 
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+usermod $USER -s /usr/bin/zsh
+
+sed -i "s/ZSH_THEME=.*/ZSH_THEME=\"cloud\"/" ~/.zshrc
+sed -i "s/# ENABLE_CORRECTION/ENABLE_CORRECTION/" ~/.zshrc
+sed -i "s/# COMPLETION_WAITING_DOTS/COMPLETION_WAITING_DOTS/" ~/.zshrc
+sed -i "s/plugins=(git)/plugins=(git composer laravel5 colored-man cp screen)/" ~/.zshrc
+source ~/.zshrc
