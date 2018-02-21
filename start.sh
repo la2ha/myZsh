e69c65143e237315a8adf9dc967daff2c7c0e212
Anton Latushka <la2ha@la2ha.com>
2018-02-22 00:26:02 +0300

    USER@HOST

--- start.sh
+++ start.sh
@@ -10,5 +10,5 @@ sed -i "s/ZSH_THEME=.*/ZSH_THEME=\"cloud\"/" ~/.zshrc
 sed -i "s/# ENABLE_CORRECTION/ENABLE_CORRECTION/" ~/.zshrc
 sed -i "s/# COMPLETION_WAITING_DOTS/COMPLETION_WAITING_DOTS/" ~/.zshrc
 sed -i "s/plugins=(git)/plugins=(git composer laravel5 colored-man cp screen)/" ~/.zshrc
-sed -i "s/PROMPT='/PROMPT='$USER@$HOST /" ~/.zshrc
+sed -i "s/PROMPT='/PROMPT='\$USER@\$HOST /" ~/.oh-my-zsh/themes/cloud.zsh-theme
 source ~/.zshrc
